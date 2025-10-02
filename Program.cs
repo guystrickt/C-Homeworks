d787402c333939091fc0f492edfcf627c5bc3e12
guystrickt <egrorvegor@gmail.com>
2025-10-02 18:35:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("---My visit card---");
+
+Console.WriteLine("My name is Egor");
+Console.WriteLine("I'm 150 years old in Telegram");
+Console.WriteLine("122 640 hours in life");
+Console.WriteLine("Yeisk");
+Console.WriteLine("gaming");
+Console.ReadLine();
+
