abd2572c571b98073d8ab0ff3a2a5066dcf0b0c5
guystrickt <egrorvegor@gmail.com>
2025-10-28 18:13:11 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,9 +1,89 @@
-﻿Console.WriteLine("---My visit card---");
+﻿using System;
 
-Console.WriteLine("My name is Egor");
-Console.WriteLine("I'm 150 years old in Telegram");
-Console.WriteLine("122 640 hours in life");
-Console.WriteLine("Yeisk");
-Console.WriteLine("gaming");
-Console.ReadLine();
+public class ArrayReverser
+{
+    public static void Main(string[] args)
+    {
+        int[] originalArray = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Исходный массив:");
+        foreach (int element in originalArray)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine("\n");
+        int[] reversedArray = new int[originalArray.Length];
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            reversedArray[i] = originalArray[originalArray.Length - 1 - i];
+        }
+        Console.WriteLine("Перевернутый массив:");
+        foreach (int element in reversedArray)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
